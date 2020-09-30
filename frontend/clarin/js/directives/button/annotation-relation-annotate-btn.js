22a09e9d9f6d44ca889dab4904fd0d2c2883891a
Clarin EL <clarinel@ellogon.iit.demokritos.gr>
2020-09-30 14:15:01 +0300

    Show update button when selecting an arrow annotation

--- frontend/clarin/js/directives/button/annotation-relation-annotate-btn.js
+++ frontend/clarin/js/directives/button/annotation-relation-annotate-btn.js
@@ -1,46 +1,46 @@
 angular.module('clarin-el').directive('relationAnnotateBtn', ['TextWidgetAPI', 'TempAnnotation',
   function(TextWidgetAPI, TempAnnotation) {
-	  return {
-		  restrict: 'E',
+    return {
+      restrict: 'E',
       templateUrl: 'templates/directives/button/annotation-relation-annotate-btn.html',
-		  scope: {
-		    annotationRelationWidgetId: '@',
+      scope: {
+        annotationRelationWidgetId: '@',
         annotationWidgetIds: '@',
         textvariable: '@'
-		  },
-		  link: function(scope, element, attrs) {
-		    // Get the <annotation-relation> element and its scope
-		    var relElem = $('#' + scope.annotationRelationWidgetId).children().first()[0];
-		    var relationScope = angular.element(relElem).scope();
-		    
-		    // Create the annotation attribute & value
-		    var annotationAttribute = {
-		      name: relationScope.annotationAttribute,
-		      value: relationScope.annotationValue
-		    }
-		    
-		    // Initialize the annotate btn variable
-		    scope.showAnnotateBtn = true;
-		    
-		    scope.addAnnotation = function() {
-		      var currentDocument = TextWidgetAPI.getCurrentDocument();
+      },
+      link: function(scope, element, attrs) {
+        // Get the <annotation-relation> element and its scope
+        var relElem = $('#' + scope.annotationRelationWidgetId).children().first()[0];
+        var relationScope = angular.element(relElem).scope();
 
-            // Create annotation object
-            var annotation = {
-              _id: new ObjectId().toString(),
-					    document_id: currentDocument.id,
-					    collection_id: currentDocument.collection_id,
-					    type: null,
-					    spans: [],
-					    attributes: [
-					      annotationAttribute
-					    ]
-            };
-		    
-		      // Get IDs of annotation comboboxes
-		      var ids = scope.annotationWidgetIds.split(' ');
-		      
-		      _.each(ids, function(id) {
+        // Create the annotation attribute & value
+        var annotationAttribute = {
+          name: relationScope.annotationAttribute,
+          value: relationScope.annotationValue
+        }
+
+        // Initialize the annotate btn variable
+        scope.showAnnotateBtn = true;
+
+        scope.addAnnotation = function() {
+          var currentDocument = TextWidgetAPI.getCurrentDocument();
+
+          // Create annotation object
+          var annotation = {
+            _id: new ObjectId().toString(),
+            document_id: currentDocument.id,
+            collection_id: currentDocument.collection_id,
+            type: null,
+            spans: [],
+            attributes: [
+              annotationAttribute
+            ]
+          };
+
+          // Get IDs of annotation comboboxes
+          var ids = scope.annotationWidgetIds.split(' ');
+
+          _.each(ids, function(id) {
             // Get div of combobox component from its id (the first child node is the div)
             var elem = $('#' + id).children().first()[0];
 
@@ -58,30 +58,53 @@ angular.module('clarin-el').directive('relationAnnotateBtn', ['TextWidgetAPI', '
               name: annotationAttribute,
               value: selectedAnnotation._id
             };
-            
+
             // Add attribute from this combobox to the annotation
             annotation.attributes.push(attribute);
-		      });
-		      
+          });
+
           TempAnnotation.save(currentDocument.collection_id, currentDocument.id, annotation)
-            .then(function(response){
+            .then(function(response) {
               if (response.success) {
                 TextWidgetAPI.addAnnotation(annotation, false);
               } else {
-                var modalOptions = { body: 'Error during saving your annotation. Please refresh the page and try again.' };
+                var modalOptions = {
+                  body: 'Error during saving your annotation. Please refresh the page and try again.'
+                };
                 Dialog.error(modalOptions);
               }
-            }, function(error){
-              var modalOptions = { body: 'Database error. Please refresh the page and try again.' };
+            }, function(error) {
+              var modalOptions = {
+                body: 'Database error. Please refresh the page and try again.'
+              };
               Dialog.error(modalOptions);
             });
-		    };
-		    
-		    scope.updateAnnotation = function() {
-		      console.log('Update annotation');
-		    }
-		  }
-	  }
+        };
+
+        scope.updateAnnotation = function() {
+          console.log('Update annotation');
+        };
+
+        var annotationSelectionUpdate = function() {
+          var selectedAnnotation = TextWidgetAPI.getSelectedAnnotation();
+
+          // For empty annotation, show the annotate button
+          if (angular.equals({}, selectedAnnotation)) {
+            // Selected annotation exists
+            scope.showAnnotateBtn = true;
+            return;
+          }
+          
+          // Check if the selected annotation concerns this button to show the update button
+          var attr = _.findWhere(selectedAnnotation.attributes, annotationAttribute);
+          
+          // Show the annotate button if we didn't find this button's attribute in the selected annotation
+          scope.showAnnotateBtn = _.isUndefined(attr);
+        }
+
+        //register callbacks for the selected annotation
+        TextWidgetAPI.registerSelectedAnnotationCallback(annotationSelectionUpdate);
+      }
+    }
   }
 ]);
-
