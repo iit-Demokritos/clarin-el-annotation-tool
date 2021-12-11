393b1fb4c014355cc289d1c7486786fe47c2f525
Georgios Petasis <petasis@iit.demokritos.gr>
2021-12-11 22:29:57 +0200

    Various Fixes: Relation Annotation

--- clarin-angular/src/app/components/controls/button/annotation-relation/annotation-relation.component.ts
+++ clarin-angular/src/app/components/controls/button/annotation-relation/annotation-relation.component.ts
@@ -11,9 +11,40 @@ export class AnnotationRelationComponent extends BaseControlComponent implements
   @Input() title;
   @Input() annotationArgumentNumber;
 
+  selected = false;
+
   super() { }
 
   ngOnInit(): void {
+    this.TextWidgetAPI.registerAnnotationSchemaCallback(this.schemaCallback.bind(this));
+    // Make sure we register the callbacks when the component loads
+    this.schemaCallback();
+  }
+
+  // Register callback for annotation updates
+  schemaCallback() {
+    this.TextWidgetAPI.registerSelectedAnnotationCallback(this.annotationSelected.bind(this));
+  }
+
+  annotationSelected() {
+    // Get the selected annotation
+    var annotation: any = this.TextWidgetAPI.getSelectedAnnotation();
+
+    // Check if the selected annotation has the same type as this combobox
+    if (annotation.type !== this.annotationType) {
+      this.selected = false;
+      return;
+    }
+
+    // Check if this annotation concerns this relation...
+    var value = this.TextWidgetAPI.getAnnotationAttributeValue(annotation, this.annotationAttribute);
+
+    // Does it match the value?
+    if (value != this.annotationValue) {
+      this.selected = false;
+      return;
+    }
+    this.selected = true;
   }
 
 }
