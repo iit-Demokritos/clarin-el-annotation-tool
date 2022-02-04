b911f89723f4ffa75981cb332e50ab98f0f242cf
Georgios Petasis <petasis@iit.demokritos.gr>
2022-02-04 10:45:25 +0200

    Dockerisation

--- docker/build.sh
+++ docker/build.sh
@@ -7,12 +7,14 @@ cd $SCRIPT_DIR
 chcon -Rt svirt_sandbox_file_t .
 setsebool -P container_manage_cgroup true
 
-echo $APACHE_RIVET_TAG
-
+# As we want to re-use the current repository (and its locla changes),
+# build image from the parent directory...
+cd ..
 docker build \
   --build-arg FEDORA_VERSION="${FEDORA_VERSION}" \
   --build-arg APACHE_RIVET_TAG="${APACHE_RIVET_TAG}" \
   --build-arg ROOT_PASSWORD="${ROOT_PASSWORD}" \
-  -t ${IMAGE_NAME}:${IMAGE_VERSION} .
+  -t ${IMAGE_NAME}:${IMAGE_VERSION} \
+  -f docker/Dockerfile .
 
 echo "Image name: ${IMAGE_NAME}:${IMAGE_VERSION}"
